4c44e2fd6897f7b4560b9c646059a519a1f7f9d9
aquafinatrippy <taneldeviis@hotmail.com>
2021-06-22 02:05:34 +0300

    basic pagination start

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -2,20 +2,34 @@
   <div v-if="loadingStatus">
     <LoadingSpinner></LoadingSpinner>
   </div>
-  <table v-else>
-    <thead>
-      <th>EESNIMI</th>
-      <th>PEREKONNANIMI</th>
-      <th>SUGU</th>
-      <th>SÜNNIKUUPÄEV</th>
-      <th>TELEFON</th>
-    </thead>
-    <tbody>
-      <tr v-for="(list, index) in getList.slice(1, 3)" :key="index">
-        <td>{{ list.firstname }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div v-else>
+    <h2>NIMEKIRI</h2>
+    <table>
+      <thead>
+        <th>EESNIMI</th>
+        <th>PEREKONNANIMI</th>
+        <th>SUGU</th>
+        <th>SÜNNIKUUPÄEV</th>
+        <th>TELEFON</th>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(list, index) in getList.slice(pagStart, pagEnd)"
+          :key="index"
+        >
+          <td>{{ list.firstname }}</td>
+          <td>{{ list.surname }}</td>
+          <td>{{ list.sex }}</td>
+          <td>{{ list.date }}</td>
+          <td>{{ list.phone }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div>
+      <button @click="PaginationNavigation('')">right</button>
+      <button @click="PaginationNavigation('forward')">left</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -24,7 +38,68 @@ import LoadingSpinner from "./LoadingSpinner.vue";
 
 export default {
   name: "Table",
+  data() {
+    return {
+      pagStart: 0,
+      pagEnd: 10,
+      active: 1,
+    };
+  },
   computed: { ...mapGetters(["getList", "loadingStatus"]) },
   components: { LoadingSpinner },
+  methods: {
+    PaginationNavigation(where) {
+      if (where === "forward") {
+        if (this.pagStart >= 0 && this.pagEnd > 0) {
+          this.pagStart += 10;
+          this.pagEnd += 10;
+        }
+      } else {
+        if (this.pagStart >= 0) {
+          this.pagStart -= 10;
+          this.pagEnd -= 10;
+        } else {
+          this.pagStart = 0;
+          this.pagEnd = 10;
+        }
+      }
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+h2 {
+  font-weight: 700;
+  font-size: 48px;
+  color: #14cc76;
+  text-align: center;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  width: 100%;
+  tr {
+    &:hover {
+      color: #ff57a2;
+    }
+  }
+  td {
+    cursor: pointer;
+  }
+}
+table td,
+table th {
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+}
+
+table th {
+  font-family: Booster;
+  color: #ff57a2;
+  text-transform: uppercase;
+  font-weight: 900;
+  text-align: left;
+  cursor: pointer;
+}
+</style>
